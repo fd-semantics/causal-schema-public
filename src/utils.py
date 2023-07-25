15be9d0e1f7758136ae17ea566e9ce6d7a4a420f
mr7723 <office@home.local>
2023-07-24 17:59:24 -0700

    change maven_topic to topic

--- src/utils.py
+++ src/utils.py
@@ -188,7 +188,7 @@ def create_graphs_from_sys_output(torquestra, generated_sys_output, resin, model
 					mention2event = dict()
 					if graph_name=='maven_graph':
 						isMaven=True
-						thisTopic = data_instance['maven_topic']
+						thisTopic = data_instance['topic']
 						#mention2event = data_instance['mention2event']
 
 					else:
